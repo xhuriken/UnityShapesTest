80c9f96c5932d5117519de9e8a3582dda863becc
xhuriken <celestin@honvault.Com>
2025-04-05 13:51:34 +0200

    Update Menu

--- Assets/CustomButtonScale.cs
+++ Assets/CustomButtonScale.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using static UnityEngine.RuleTile.TilingRuleOutput;
+using Shapes;
 
 public class CustomButtonScale : CustomButtonBase
 {
@@ -11,37 +12,49 @@ public class CustomButtonScale : CustomButtonBase
     [SerializeField] private float toScale;
     [SerializeField] private float duration;
     [SerializeField] private float moveDistance = 1f;
-
+    [SerializeField] private float ballAnimDuration = 0.5f;
+    [SerializeField] private float ballTargetRadius = 15f;
     private GameObject _text;
     private GameObject _ball;
     private Animator _ballAnim;
-
+    private Disc _ballDisc;
     private void Start()
     {
         _text = transform.GetChild(0).gameObject;
         _ball = transform.GetChild(1).gameObject;
         _ballAnim = _ball.GetComponent<Animator>();
-
+        _ballDisc = _ball.GetComponent<Disc>();
         _originalTextPos = _text.transform.localPosition;
+
+        _ballDisc.Radius = 0f;
     }
 
     public override void OnPointerEnter(PointerEventData eventData)
     {
         base.OnPointerEnter(eventData);
 
-        _ballAnim.SetTrigger("Show");
+        //_ballAnim.SetTrigger("Show");
 
         _text.transform.DOScale(toScale, duration).SetEase(Ease.InOutSine);
         _text.transform.DOLocalMoveX(_originalTextPos.x + moveDistance, duration).SetEase(Ease.InOutSine);
+
+
+        DOTween.To(() => _ballDisc.Radius, x => _ballDisc.Radius = x, ballTargetRadius, ballAnimDuration)
+                .SetEase(Ease.OutBounce);
+        _ball.transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.InOutSine);
     }
 
     public override void OnPointerExit(PointerEventData eventData)
     {
         base.OnPointerExit(eventData);
 
-        _ballAnim.SetTrigger("Mask");
+        //_ballAnim.SetTrigger("Mask");
 
         _text.transform.DOScale(OriginalScale, duration).SetEase(Ease.InOutSine);
         _text.transform.DOLocalMoveX(_originalTextPos.x, duration).SetEase(Ease.InOutSine);
+
+        DOTween.To(() => _ballDisc.Radius, x => _ballDisc.Radius = x, 0f, ballAnimDuration)
+                .SetEase(Ease.InBack);
+        _ball.transform.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InOutSine);
     }
 }
